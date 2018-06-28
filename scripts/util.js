587786f1d1fe2cc7fc60ef66974ef8eb0daf18fd
bablet <frk.haha@hotmail.se>
2018-06-28 03:26:27 +0200

    fix: Animations

--- scripts/util.js
+++ scripts/util.js
@@ -5,8 +5,10 @@ const top_page  = document.querySelector('.top_page');
 export const listen = () => {
     welcome.setAttribute('data-state', 'start');
 
-    welcome.addEventListener('animationend', e => {
-        root.removeChild(welcome);
-        top_page.setAttribute('data-state', 'start');
+    welcome.querySelector(".welcome_top").addEventListener('animationend', e => {
+        if(e.animationName === "welcome_top_slide" ){
+            root.removeChild(welcome);
+            top_page.setAttribute('data-state', 'start');
+        }
     });
 }
