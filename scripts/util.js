fe637df7c08f98787b4fb8ca94be91f4392ba7ad
bablet <frk.haha@hotmail.se>
2018-06-30 21:30:00 +0200

    update: page transition

--- scripts/util.js
+++ scripts/util.js
@@ -1,6 +1,18 @@
-const root      = document.getElementById('root');
-const welcome   = document.querySelector('.welcome');
-const top_page  = document.querySelector('.top_page');
+const root                  = document.getElementById('root');
+const welcome               = document.querySelector('.welcome');
+const top_page              = document.querySelector('.top_page');
+const top_page__navigation  = document.querySelector('.top_page__navigation');
+
+export const bindButtons = () => {
+    const buttons = document.querySelectorAll('.top_page__navigation a');
+
+    buttons.forEach((nav, key) => {
+        nav.addEventListener('click', e => {
+            e.preventDefault();
+            //Implement
+        });
+    });
+}
 
 export const listen = () => {
     welcome.setAttribute('data-state', 'start');
